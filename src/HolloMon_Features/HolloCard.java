111fa721bbdf1586665c38868c85e07672dccd27
Albert‎ <253417509+PotVal1ant@users.noreply.github.com>
2026-03-25 19:54:59 +0000

    Cards done?

---
+++ src/HolloMon_Features/HolloCard.java
@@ -0,0 +1,29 @@
+package HolloMon_Features;
+
+enum CardRank {
+    COMMON, UNCOMMON, RARE, UNIQUE;
+}
+
+
+class HolloCard{
+
+
+
+  private Integer cardID;
+  private String name;
+  private CardRank rank;
+  private Integer last_price;
+
+
+  HolloCard(card_ID, name, rank, last_price)
+  {
+    this.id = id;
+    this.name = name;
+    this.rank = rank;
+    this.last_price = last_price;
+  }
+
+
+
+
+};
